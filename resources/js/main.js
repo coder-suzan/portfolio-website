5981a23ae339dad8db04ee757cbbf33052480349
coder-suzan <79319220+coder-suzan@users.noreply.github.com>
2021-04-29 00:54:03 +0600

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	$('#wordpress').LineProgressbar({
+  percentage: 100,
+  fillBackgroundColor: '#FF4200',
+  backgroundColor: '#707070',
+  radius: '0px',
+  height: '7px',
+  width: '100%'
+});	
+	$('#html').LineProgressbar({
+  percentage: 90,
+  fillBackgroundColor: '#FF7004',
+  backgroundColor: '#707070',
+  radius: '0px',
+  height: '7px',
+  width: '100%'
+});	
+	
+	$('#php').LineProgressbar({
+  percentage: 85,
+  fillBackgroundColor: '#FF9809',
+  backgroundColor: '#707070',
+  radius: '0px',
+  height: '7px',
+  width: '100%'
+});	
+	
+	$('#css').LineProgressbar({
+  percentage: 95,
+  fillBackgroundColor: '#FFBF0D',
+  backgroundColor: '#707070',
+  radius: '0px',
+  height: '7px',
+  width: '100%'
+});	
+
+
+	//FILTER-DATA MIXITUP PLUGIN
+	var mixer = mixitup('#container');
+	
+});
